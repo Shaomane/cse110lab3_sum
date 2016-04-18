9c4db8f18ca51144944b2a70e8111cf3e4f17e02
Noel Lin <nolin9889@gmail.com>
2016-04-18 16:56:28 -0700

    Failing in android studio but passing in circleci

--- app/src/main/java/com/example/noellin/lab3exercises/MainActivity.java
+++ app/src/main/java/com/example/noellin/lab3exercises/MainActivity.java
@@ -70,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
     public int sum(int num1, int num2) {
 
-        return num1 - num2;
+        return (num1 - num2);
     }
 /*    public int sub(int num1, int num2) {
         return num1 - num2;
